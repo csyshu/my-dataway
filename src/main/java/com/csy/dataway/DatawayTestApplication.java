9d16caf9adbc89d5205801cbd694dd9343da45fe
shuyun.cheng <shuyun.cheng@100credit.com>
2021-04-10 11:48:07 +0800

    :bento: 更新相关配置及jar包

---
+++ src/main/java/com/csy/dataway/DatawayTestApplication.java
@@ -0,0 +1,13 @@
+package com.csy.dataway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawayTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatawayTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csy/dataway/DatawayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csy.dataway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawayTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
